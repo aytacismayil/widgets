d7afb5917409948d19dc6626e5424431823d20c7
aytacismayil <aytaj.ismayill@gmail.com>
2022-05-15 17:48:46 +0400

    Dropdown

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,25 +1,90 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
-  const [term, setTerm] = useState("");
+  const [term, setTerm] = useState("programming");
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
+  const [results, setResults] = useState([]);
 
   useEffect(() => {
-      console.log("a");
-  }, [])
+    const timeId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
+    return () => {
+      clearTimeout(timeId);
+    };
+  }, [term]);
+
+  useEffect(() => {
+    console.log("Initial render or term was changed");
+
+    const search = async () => {
+      const response = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: debouncedTerm
+        }
+      });
+
+      setResults(response.data.query.search);
+    };
+    search();
+  }, [debouncedTerm]);
+
+  console.log(results);
+
+  // useEffect(() => {
+  //   if (term && !results.length) {
+  //     search();
+  //   } else {
+  //     const timeoutId = setTimeout(() => {
+  //       if (term) {
+  //         search();
+  //       }
+  //     }, 500);
+
+  //     return () => {
+  //       clearTimeout(timeoutId);
+  //     };
+  //   }
+  // }, [term, results.length]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+          {/* {result.snippet} */}
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div>
       <div className="ui form">
         <div className="field">
-          <label htmlFor="">Enter Search Term</label>
+          <label>Enter Search Term</label>
           <input
-            type="text"
-            className="input"
             value={term}
             onChange={(e) => setTerm(e.target.value)}
+            className="input"
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
